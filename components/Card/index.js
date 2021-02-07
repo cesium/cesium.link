50366a64df2c32e11707f0d37a5ab33992caf5d9
Nelson Estevão <nelsonmestevao@gmail.com>
2021-02-07 16:09:34 +0000

    Make emojis more accessible

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,12 +1,12 @@
-import 'animate.css';
+import Emoji, { Twemoji, Emojione } from 'react-emoji-render';
 
+import 'animate.css';
 import styles from "./style.module.css";
 
-const Card = ({ title, description, emoji, url, atention=false }) => (
+const Card = ({ title, description, emoji, url, atention = false }) => (
   <a href={url} className={styles.card}>
     <h3 className={atention && "animate__animated animate__shakeX animate__delay-2s"}>
-
-      {emoji} {title}
+      <Twemoji className="false" svg text={`${emoji} ${title}`} />
     </h3>
     {description && <p>{description}</p>}
   </a>
