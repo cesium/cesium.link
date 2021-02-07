1f39d33ea59a6c48fe2b5920b4049aab6cfdd697
Nelson Estevão <nelsonmestevao@gmail.com>
2021-02-07 04:14:56 +0000

    Add animation for seek atention

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,8 +1,11 @@
+import 'animate.css';
+
 import styles from "./style.module.css";
 
-const Card = ({ title, description, emoji, url }) => (
+const Card = ({ title, description, emoji, url, atention=false }) => (
   <a href={url} className={styles.card}>
-    <h3>
+    <h3 className={atention && "animate__animated animate__shakeX animate__delay-2s"}>
+
       {emoji} {title}
     </h3>
     {description && <p>{description}</p>}
