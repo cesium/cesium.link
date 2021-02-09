d4ac119fdd9b2a80eed6e1f4c87c71639bf92a86
Nelson Estevão <nelsonmestevao@gmail.com>
2021-02-09 17:21:27 +0000

    Remove warnings

--- components/Admin/LinksTable.js
+++ components/Admin/LinksTable.js
@@ -8,74 +8,82 @@ import NewEntry from "../../components/Admin/NewEntry";
 import API from "../../utils/api";
 
 const columns = [
-    {
-        title: "Emoji",
-        dataIndex: "emoji",
-        key: "emoji",
-        fixed: "left",
-        align: "center",
-        render: (emoji) => <Twemoji svg text={emoji} />,
+  {
+    title: "Emoji",
+    dataIndex: "emoji",
+    key: "emoji",
+    fixed: "left",
+    align: "center",
+    render: function Emoji(emoji) {
+      return <Twemoji svg text={emoji} />;
     },
-    {
-        title: "Title",
-        dataIndex: "title",
-        key: "title",
+  },
+  {
+    title: "Title",
+    dataIndex: "title",
+    key: "title",
+  },
+  {
+    title: "Description",
+    dataIndex: "description",
+    key: "description",
+  },
+  {
+    title: "Attention",
+    dataIndex: "attention",
+    key: "attention",
+    align: "center",
+    render: function Attention(state) {
+      return <Checkbox checked={state} disabled={true} />;
     },
-    {
-        title: "Description",
-        dataIndex: "description",
-        key: "description",
+  },
+  {
+    title: "URL",
+    dataIndex: "url",
+    key: "url",
+    render: function Url(url) {
+      return <a href={url}>{url}</a>;
     },
-    {
-        title: "Attention",
-        dataIndex: "attention",
-        key: "attention",
-        align: "center",
-        render: (state) => <Checkbox checked={state} disabled={true} />,
+  },
+  {
+    title: "Action",
+    key: "action",
+    fixed: "right",
+    render: function Action(_, record) {
+      return <Actions record={record} />;
     },
-    {
-        title: "URL",
-        dataIndex: "url",
-        key: "url",
-        render: (url) => <a href={url}>{url}</a>,
-    },
-    {
-        title: "Action",
-        key: "action",
-        fixed: "right",
-        render: (_, record) => <Actions record={record} />,
-    },
+  },
 ];
 
 function LinksTable() {
-    const [loading, setLoading] = useState(true);
-    const { links, dispatch } = useLinks();
+  const [loading, setLoading] = useState(true);
+  const { links, dispatch } = useLinks();
 
-    useEffect(() => {
-        API.get("/links")
-            .then((response) => {
-                dispatch({ type: "INIT", links: response.data.data.reverse() });
-                setLoading(false)
-            })
-            .catch((error) => {
-                notification["error"]({
-                    message: `${error.response.statusText}`,
-                    description: error.message,
-                });
-            });
-    }, []);
+  useEffect(() => {
+    API.get("/links")
+      .then((response) => {
+        dispatch({ type: "INIT", links: response.data.data.reverse() });
+        setLoading(false);
+      })
+      .catch((error) => {
+        notification["error"]({
+          message: `${error.response.statusText}`,
+          description: error.message,
+        });
+      });
+  }, []);
 
-    return (
-        <Table
-            columns={columns}
-            loading={loading}
-            bordered
-            rowKey="_id"
-            pagination={{ position: ["bottomCenter"] }}
-            dataSource={links}
-            footer={() => <NewEntry />}
-        />
-    );
+  return (
+    <Table
+      columns={columns}
+      loading={loading}
+      bordered
+      rowKey="_id"
+      pagination={{ position: ["bottomCenter"] }}
+      dataSource={links}
+      footer={() => <NewEntry />}
+    />
+  );
 }
 
 export default LinksTable;
