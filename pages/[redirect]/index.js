65118f7f7e401359300101cde3f9379859faeab3
Nelson Estevão <nelsonmestevao@gmail.com>
2021-03-13 20:25:13 +0000

    Redirect to CeSIUM website (#11)

---
+++ pages/[redirect]/index.js
@@ -0,0 +1,21 @@
+import ErrorPage from 'next/error';
+
+import { domain } from '../../data/settings.yml';
+
+export async function getServerSideProps({ params }) {
+  const { redirect } = params;
+
+  return {
+    redirect: {
+      destination: `${domain}/${redirect}`,
+      permanent: false
+    }
+  };
+
+}
+
+const Redirect = ({ message = 'This page could not be found.', code = 404 }) => (
+  <ErrorPage title={message} statusCode={code} />
+);
+
+export default Redirect;
