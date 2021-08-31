d5ca93117c365d84df6fd5ccd763220343f84fc0
Nelson Estevão <nelsonmestevao@gmail.com>
2021-08-31 23:52:49 +0100

    Remove APP URL for easier deployment in different environments (#24)

--- lib/api.ts
+++ lib/api.ts
@@ -1,13 +1,6 @@
 import axios from 'redaxios';
 
-let APP_URL = process.env.NEXT_PUBLIC_APP_URL;
-
-if (!/^https?:\/\//i.test(APP_URL)) {
-  APP_URL = 'http://' + APP_URL;
-}
-
 const API = axios.create({
-  baseURL: `${APP_URL}`,
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json'
