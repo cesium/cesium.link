6b59fe23cd819dd9e929925cc97707d4a22683d8
Nelson Estevão <nelsonmestevao@gmail.com>
2021-11-01 14:35:29 +0000

    Test if we can redirect first (#27)

--- pages/[redirect]/index.tsx
+++ pages/[redirect]/index.tsx
@@ -1,14 +1,32 @@
 import { GetServerSideProps } from 'next';
 import ErrorPage from 'next/error';
+import API from '~/lib/api';
 
 import { domain } from '~/data/settings.json';
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const { redirect } = params;
 
+  const response = await API.get(`/api/r/${redirect}/url`)
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      return error;
+    });
+
+  if (!response.data.success) {
+    return {
+      redirect: {
+        destination: `${domain}/${redirect}`,
+        permanent: false
+      }
+    };
+  }
+
   return {
     redirect: {
-      destination: `${domain}/${redirect}`,
+      destination: response.data.data,
       permanent: false
     }
   };
