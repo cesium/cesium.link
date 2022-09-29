dfad0505340824f832196b662f5c83fed2160770
Nelson Estevão <nelsonmestevao@proton.me>
2022-09-29 13:13:00 +0100

    Add page to generate QR Code on demand (#63)

---
+++ pages/admin/qrcodes.tsx
@@ -0,0 +1,42 @@
+import { LinkOutlined } from '@ant-design/icons';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { Input, Row, Space } from 'antd';
+import { useState } from 'react';
+import { QRCode } from 'react-qrcode-logo';
+
+import AdminLayout from '~/components/AdminLayout';
+
+function Page() {
+  const [url, setUrl] = useState<string>('');
+
+  return (
+    <Row style={{ paddingTop: '20px' }} align="middle" justify="center">
+      <Space align="center" direction="vertical" size="large">
+        <Input
+          size="large"
+          placeholder="large size"
+          prefix={<LinkOutlined />}
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        {url && (
+          <QRCode
+            value={url}
+            logoImage={'/logo.png'}
+            fgColor="#ff794d"
+            removeQrCodeBehindLogo
+            size={500}
+          />
+        )}
+      </Space>
+    </Row>
+  );
+}
+
+Page.getLayout = function getLayout(page) {
+  return <AdminLayout tab="qrcodes">{page}</AdminLayout>;
+};
+
+export const getServerSideProps = withPageAuthRequired();
+
+export default Page;
