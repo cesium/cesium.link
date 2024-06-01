4d9150ef6cfaae2280050c8081d210281a427bb8
João Lobo <joao@lobo.is>
2024-06-01 14:45:51 +0100

    feat: socials

--- lib/cesium_link_web/components/footer.ex
+++ lib/cesium_link_web/components/footer.ex
@@ -3,8 +3,24 @@ defmodule CesiumLinkWeb.Components.Footer do
 
   def footer(assigns) do
     ~H"""
-    <footer>
-      <.link href="https://cesium.di.uminho.pt" target="_blank" class="my-1 flex select-none items-center py-4">
+    <footer class="border-t-[1px] border-[#eaeaea] w-full py-5">
+      <!-- Socials -->
+      <div class="flex gap-6 justify-center w-full pb-2">
+        <.link href="https://instagram.com/cesiuminho" target="_blank">
+          <img src="/images/socials/instagram.svg" width="24" height="24" alt="Instagram" />
+        </.link>
+        <.link href="https://facebook.com/cesiuminho" target="_blank">
+          <img src="/images/socials/facebook.svg" width="24" height="24" alt="Facebook" />
+        </.link>
+        <.link href="https://x.com/cesiuminho" target="_blank">
+          <img src="/images/socials/x.svg" width="24" height="24" alt="X" />
+        </.link>
+        <.link href="https://github.com/cesium" target="_blank">
+          <img src="/images/socials/github.svg" width="24" height="24" alt="GitHub" />
+        </.link>
+      </div>
+      <!-- Credits -->
+      <.link href="https://cesium.pt" target="_blank" class="w-full justify-center flex select-none items-center py-4">
         hacked with 🧡 by <img src="/images/cesium-logo.svg" width="84" height="24" alt="CeSIUM" class="ml-2" />
       </.link>
     </footer>
