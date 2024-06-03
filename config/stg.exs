b3be8c127bc3e1e7db166be13e35fba66baeaec7
João Lobo <joao@lobo.is>
2024-06-03 17:44:25 +0100

    feat: deployment config

---
+++ config/stg.exs
@@ -0,0 +1,11 @@
+import Config
+
+# ## SSL Support
+#
+# To get SSL working, you will need to add the `https` key
+# to the previous section and set your `:url` port to 443:
+#
+config :cesium_link, CesiumLink.Endpoint,
+  url: [scheme: "https", host: "cesium-link.fly.dev", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json"
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\cesium_link" eval CesiumLink.Release.migrate
