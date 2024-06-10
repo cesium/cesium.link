76fdfb336b0ca5ca7304135c9c40c4e0138722b7
João Lobo <joao@lobo.is>
2024-06-10 01:22:19 +0100

    chore: add deployment scripts & staging environment config

--- config/stg.exs
+++ config/stg.exs
@@ -6,6 +6,6 @@ import Config
 # to the previous section and set your `:url` port to 443:
 #
 config :cesium_link, CesiumLink.Endpoint,
-  url: [scheme: "https", host: "cesium-link.fly.dev", port: 443],
+  url: [scheme: "https", host: "stg.cesium.link", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
