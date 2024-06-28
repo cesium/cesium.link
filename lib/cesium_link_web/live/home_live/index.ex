65380ac8f37753cc1bada6239efe571059faff20
João Lobo <joao@lobo.is>
2024-06-28 18:59:19 +0100

    refactor: use streams on home page

--- lib/cesium_link_web/live/home_live/index.ex
+++ lib/cesium_link_web/live/home_live/index.ex
@@ -7,15 +7,14 @@ defmodule CesiumLinkWeb.HomeLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, stream(socket, :links, Links.list_unarchived_links_by_index())}
   end
 
   @impl true
   def handle_params(_params, _url, socket) do
     {:noreply,
      socket
-     |> assign(:page_title, "CeSIUM")
-     |> assign(:links, Links.list_unarchived_links_by_index())}
+     |> assign(:page_title, "CeSIUM")}
   end
 
   @impl true
