3187021919b26d72a0313fdff9b92ead3310c418
João Lobo <joaodlobo@gmail.com>
2024-08-22 23:41:30 +0100

    ops: production environment

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :cesium_link, CesiumLinkWeb.Endpoint,
+url: [scheme: "https", host: "cesium.link", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
