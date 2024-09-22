4c774262d919507edc4070a9ab42ab9b4a49fc9f
Afonso Martins <afonsopm2005@gmail.com>
2024-09-22 20:24:41 +0100

    requested changes links.ex

--- lib/cesium_link/links.ex
+++ lib/cesium_link/links.ex
@@ -66,7 +66,7 @@ defmodule CesiumLink.Links do
   def list_unarchived_links_by_index do
     Link
     |> where([l], fragment("? <= now() OR ? IS NULL", l.publish_at, l.publish_at))
-    |> where([l], l.archived == false)
+    |> where([l], not l.archived)
     |> order_by([l], asc: l.index)
     |> Repo.all()
   end
