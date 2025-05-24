53fd9011029f14d565aab0611a8cd1f42630a5fd
Nuno Miguel <nmpf.2005@gmail.com>
2025-05-24 19:29:42 +0100

    chore: adjust config to allow ipv4 connections on prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -74,7 +74,6 @@ if config_env() in [:prod, :stg] do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base
